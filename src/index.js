6609e9c07179aecba257c5c700a52afe12e3646e
Christopher Amanor <kriskit7@gmail.com>
2020-08-13 10:57:38 +0000

    :hammer: Fix responsiveness of page on all device screen sizes

--- src/index.js
+++ src/index.js
@@ -4,25 +4,28 @@ const content = document.querySelector('#content');
 
 homePageLoad(content);
 
-const handleClick = () => {
-  document
-    .getElementById('menu-list')
-    .addEventListener('click', (e) => render(content, e));
-};
-
 const render = (content, e) => {
   const clickedItem = e.target.innerHTML;
 
   if (clickedItem === 'CONTACT US') {
     contactPageLoad(content);
+    // eslint-disable-next-line no-use-before-define
     handleClick();
   } else if (clickedItem === 'MENU') {
     menuPageLoad(content);
+    // eslint-disable-next-line no-use-before-define
     handleClick();
   } else {
     homePageLoad(content);
+    // eslint-disable-next-line no-use-before-define
     handleClick();
   }
+};
+
+const handleClick = () => {
+  document
+    .getElementById('menu-list')
+    .addEventListener('click', (e) => render(content, e));
 };
 
 handleClick();
